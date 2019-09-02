a2e40c02b6aee8ac5e43a3f8505078984154a034
Sakinder <sakinder@live.com>
2019-09-01 20:30:34 -0400

    Cleaned Folders
    Cleaned Folders

--- ZEDBOARD/workspace/sdk/vfpWebCamera/src/D5M/MENU_CALLS/menu_calls.h
+++
@@ -1,5 +0,0 @@
-// LAST TESTED : 12/16/2018
-#include <xil_types.h>
-u32 uartcmd(u32 argA,u32 argB);
-void menu_calls(int ON_OFF);
-int sd_card();
--- ZEDBOARD/workspace/sdk/vfpWebCamera/src/D5M/PLATFORM/platform.h
+++
@@ -1,6 +0,0 @@
-//#ifndef __PLATFORM_H__
-//#define __PLATFORM_H__
-//
-//void init_platform();
-//void cleanup_platform();
-//#endif // __PLATFORM_H__
